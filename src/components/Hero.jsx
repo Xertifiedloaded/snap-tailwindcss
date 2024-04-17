272b38690de69c227bc277b35b809491e463f792
xertifiedloaded <certifiedloaded@yahoo.com>
2024-04-17 10:29:40 +0100

    my changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import icon from '../assets/images/image-hero-desktop.png'
 export default function Hero() {
     return (
         <>
-            <section className='w-[80%] sm:w-[90%] sm:mx-auto mx-auto flex items-center justify-between sm:flex-col-reverse '>
+            <section className=' w-[80%] sm:w-[90%] sm:mx-auto mx-auto flex items-center justify-between sm:flex-col-reverse '>
                 <div className='w-[50%] sm:w-[100%] sm:text-center my-[20px]'>
                     <h1 className='text-[70px] md:text-[35px] lg:text-[35px] sm:text-[2rem] font-bold'>Make <br className='sm:hidden' /> Remote Work</h1>
                 </div>
--- src/components/ui/Head.jsx
+++ src/components/ui/Head.jsx
@@ -4,55 +4,37 @@ import { Nav } from '../../utils/nav';
 import logo from '../../assets/images/logo.svg'
 import hamburger from '../../assets/images/icon-menu.svg'
 import close from '../../assets/images/icon-close-menu.svg'
+import { Link } from 'react-router-dom';
 export default function Head() {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [active, setActive] = useState(false)
-  const toggler = () => {
-    setActive(!active)
+
+  let [isOpen, setIsOpen] = useState(false)
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen)
   }
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
   return (
-    <>
-      <header className='w-width relative mx-auto flex items-center  gap-[40px] justify-between py-3'>
-        <div className='w-[5%] sm:w-[20%]'>
-          <img src={logo} alt="" />
-        </div>
-        <nav
-          className={`flex justify-between md:text-[14px] w-[95%] 
-        ${active ? 'sm:justify-start flex sm:items-start sm:top-10 sm:h-[100vh] sm:mx-0 z-10  sm:absolute sm:w-[100%] sm:flex-col sm:px-[20px] sm:py-[50px] bg-black sm:text-white' : 'sm:hidden'} 
-        items-center`}>
-          <ul className='flex sm: sm:flex-col  gap-4'>
-            {Nav.map((navItem, index) => (
-              <li className='text capitalize' key={index} onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
-                {navItem.tag}
-                {navItem.dropdown && showDropdown && (
-                  <ul className='dropdown-content'>
-                    {navItem.dropdown.map((dropdownItem, index) => (
-                      <li className='text-gray' key={index}>{dropdownItem.content}</li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-          <div className="flex sm: sm:flex-col  items-center sm:items-start gap-4 sm:mt-4">
-            <button>
-              Login
-            </button>
-            <button className='border border-black py-2 rounded-lg sm:w-full px-6 sm:px-0'>
-              Signup
-            </button>
-          </div>
-        </nav>
-        <nav className='hidden sm:block' onClick={toggler}>
-          {
-            active ? <img src={close} alt="" /> : <img src={hamburger} alt="" />
-          }
-        </nav>
-      </header>
-    </>
-  );
+    <header className='px-10 py-4 shadow-md w-full m-auto flex items-center sm:z-[99] md:z-[99]  static top-0 bg-white'>
+    <div className='font-bold text-2xl'>Snap</div>
+    <div onClick={toggleIsOpen} className='menu w-7 h-7 absolute right-8 top-6 cursor-pointer xl:hidden lg:hidden'>
+      {isOpen ? <img src={close} alt='' /> : <img src={hamburger} alt='' />}
+    </div>
+    <nav
+      className={`flex justify-between w-[95%] sm:bg-black md:bg-black md:text-white 
+        ${isOpen ? 'sm:top-12' : 'sm:top-[-490px] md:top-[-490px]'}
+        ${isOpen ? 'md:top-12' : 'md:top-[-490px]'}  
+        sm:text-white sm:justify-normal md:justify-normal items-center sm:absolute md:absolute sm:top-16 md:top-16 
+        sm:flex-col md:flex-col sm:z-[2] md:z-[-1] md:left-0 md:w-full sm:left-0 sm:w-[100%] transition-all duration-500 ease-in`}
+    >
+      <ul className='flex text-[15px] justify-between pl-9 sm:pl-0 md:pl-0 md:w-[100%] sm:flex-col md:flex-col sm:w-[100%]'>
+        {Nav.map((link, index) => (
+          <li key={index} className='md:my-2 sm:my-4'>
+            <Link to='/' className='my-7 capitalize ml-8 md:my-0 font-semibold'>
+              {link.tag}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </header>
+  )
 }
 
